908a30bdc1f06a9e39daf1a961031ba8380a2271
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-09-17 02:20:20 +0200

    ejercicios hechos hasta el 4 inclusive

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -7,7 +7,7 @@ Write JavaScript below that logs:
 
     2. the first div element node
     --> should log the ".site-header" node
-
+    
     3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node
 
@@ -16,6 +16,28 @@ Write JavaScript below that logs:
 
 */
 
+// 1.
+console.log(document.querySelectorAll("p"))
+let pArray = Array.from(document.querySelectorAll("p"));
+pArray.forEach((i) => {
+    console.log(i)
+        //    todoList.appendChild(generateTodoDOM(todo))
+    })
+    
+
+//2.
+let divArray = Array.from(document.querySelectorAll("div"));
+console.log(divArray[0])
+
+//3.
+console.log(document.querySelector("#jumbotron-text"));
+
+//4.
+// let prim_cont = document.querySelector(".primary-content");
+// let p_in_prim = prim_cont.querySelectorAll("p");
+// console.log(p_in_prim);
+
+console.log(document.querySelector(".primary-content").querySelectorAll("p"));
 
 /*
 Task 2
@@ -23,6 +45,14 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+
+        
+let myButton = document.querySelector("#alertBtn");
+myButton.addEventListener("click", alertSomething);
+
+function alertSomething() {
+  alert("Thanks for visiting Bikes for Refugees!");
+}
 
 
 /*
@@ -32,6 +62,17 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
+let changeBtn = document.querySelector("#bgrChangeBtn");
+changeBtn.addEventListener("click", changecolour);
+
+
+function changecolour() {
+    let body = document.querySelector("body");
+    //  let body = document.body;
+    body.style.backgroundColor = "#abceeb";
+    // document.body.style.backgroundColor = "red";
+
+}
 
 /*
 Task 4
@@ -40,7 +81,15 @@ Task 4
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
 */
 
+let addTxt = document.querySelector("#addTextBtn");
+addTxt.addEventListener("click", addText);
 
+function addText(){
+    let paragraph = document.createElement("p");
+    let mainArticles = document.querySelector("#mainArticles");    
+    mainArticles.appendChild(paragraph);
+    paragraph.innerText = "NEW PARAGRAPH"; 
+}
 
 /*
 Task 5
@@ -48,6 +97,15 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+// let largestLinkBtn = document.querySelector("#largestLinksBtn");
+// largestLinkBtn.addEventListener("click", largestLinks);
+
+// function largestLinks() {
+    // let fLink = document.querySelector(".article-title");
+    // fLink.toUpperCase
+    
+
+
 
 
 /*
@@ -58,7 +116,18 @@ Using the same function in Task 4,
 When the 'Add' button is clicked, get the text inside the input field and create a new paragraph in the "LEARN MORE" section
 Also clear the text inside the input field
 */
+/*let addTxto = document.querySelector("#addArticleBtn");
+addTxto.addEventListener("click", addTexto);
 
+function addTexto(){
+    let paragraph = document.createElement("p");
+    paragraph.classList.add('Add some text here');
+    paragraph.textContent = 'There are no todos to show'
+    todoList.appendChild(paragraph);
+    let form = document.querySelector(".form-control");    
+    form.appendChild(paragraph);
+    paragraph.innerText = "NEW PARAGRAPH2"; 
+}
 /*
 Task 7
 ======
