40c52e1e02c828ca0e641ce0c557756d01a90e2d
Natalia Tamara Rosa <tamararosa6@gmail.com>
2022-10-01 12:56:48 +0200

    ejercicio G hecho

--- week-2/Homework/Part-2/G-object-loops/exercise-1.js
+++ week-2/Homework/Part-2/G-object-loops/exercise-1.js
@@ -8,6 +8,17 @@ const studentGrades = {
     abdul: 19,
   };
   
+  const studentKey = Object.keys(studentGrades);
+  const studentvalues = Object.values(studentGrades);
+  
+  studentvalues.forEach((studentvalues, key) =>{
+    if (studentvalues > 18 ){
+      //LLamo a la variable de las llaves y le agrego la mayus. y coloco los values
+    console.log(`${studentKey[key].toUpperCase()} - ${studentvalues}`);
+    }
+})
+
+
   // Prints
   // TOM - 20
   // ABDUL - 19
